4946711f63ce181d9185a0fe22f497e778d6e185
nicolasmcmory <nicolasmcmory@gmail.com>
2026-01-25 23:04:29 -0500

    Implement monthly challenges feature with views, services, and templates

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.index)]
+urlpatterns = [
+    path("", views.challenges, name="challenges"),
+    path("<int:month>", views.challenges_by_month_int),
+    path("<str:month>", views.challenges_by_month_str, name="challenges_by_month"),
+]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from .services import Month_challenges
+from django.template.loader import render_to_string
 
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello world!")
+# Views
+def challenges_by_month_str(request, month: str):
+    # Init month challenges
+    month_challenges = Month_challenges()
+
+    try:
+        challenge = month_challenges.get_challenge_str(month)
+        response_data = render_to_string("challenges/challenge.html")
+        return HttpResponse(response_data)
+    
+    except Exception as e:
+        return HttpResponseNotFound(f"Month doesn't exist. Cause: {e}")
+
+
+
+def challenges_by_month_int(request, month: int):
+    # Init month challenges
+    month_challenges = Month_challenges()
+    month = month_challenges.get_month_from_num(month)
+
+    # Test if month is Exception
+    if isinstance(month, Exception):
+        return HttpResponseNotFound(month)
+
+    else:
+        redirect_str = reverse("challenges_by_month", args=[month])
+        return HttpResponseRedirect(redirect_str)
+
+
+def challenges(request):
+    # Init month challenges
+    months = Month_challenges().get_list_months()
+
+    response_content = "<ul>"
+
+    for month in months:
+        month_link = reverse("challenges_by_month", args=[month])
+        response_content += (
+            f'<li><a href = "{month_link}">{month.capitalize()}</a></li>'
+        )
+
+    response_content += "</ul>"
+
+    return HttpResponse(response_content)
