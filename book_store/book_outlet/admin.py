83bc866997eb01623c51ebeaa5aa4b720be28c02
nicolasmcmory <nicolasmcmory@gmail.com>
2026-02-14 17:16:24 -0500

    Add Author model and update Book model to use ForeignKey for author relationship

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -8,3 +8,4 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,13 +4,29 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+# Data relations
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    # Dunder method, override
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
 # id field is set automatically by Django
 class Book(models.Model):
     title = models.CharField(max_length=50, default="no_title")
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)], default=1
     )
-    author = models.CharField(null=True, max_length=100)
+    # Relation field
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books"
+    )
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(
         default="", null=False, db_index=True
@@ -19,5 +35,6 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
 
+    # Dunder method, overrride
     def __str__(self):
         return f"Title: {self.title}, Author: {self.author} Rating: {self.rating}"
