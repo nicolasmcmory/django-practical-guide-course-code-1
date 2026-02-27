78ea00ec331b89637dbfb1ec9b9b7d8f1515e695
nicolasmcmory <nicolasmcmory@gmail.com>
2026-02-27 16:57:47 -0500

    Progress

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -1,5 +1,7 @@
-# urls: reviews
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.review, name="reviews")]
+urlpatterns = [
+    path("", views.review, name="reviews"),
+    path("thank-you", views.thank_you, name="thank-you"),
+]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,7 +1,22 @@
-#views: reviews
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import ReviewForm
+
 
 # Create your views here.
 def review(request):
-    return render(request,"reviews/review.html")
+    has_error_false = {"has_error": False}
+    if request.method == "POST":
+        entered_username = request.POST["username"]
+        print(entered_username)
+
+        if entered_username == "":
+            return render(request, "reviews/review.html", {"has_error": True})
+
+        return HttpResponseRedirect("/thank-you")
+
+    return render(request, "reviews/review.html", has_error_false)
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
