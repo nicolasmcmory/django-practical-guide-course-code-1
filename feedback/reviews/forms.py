f7b8ae6c206c188688340f38fbac0eeed7126770
nicolasmcmory <nicolasmcmory@gmail.com>
2026-03-01 01:38:08 -0500

    Progress

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your name", max_length=100)
+    user_name = forms.CharField(label="Your name", max_length=50)
     review_text = forms.CharField(
         label="Your feedback", widget=forms.Textarea, max_length=200
     )
