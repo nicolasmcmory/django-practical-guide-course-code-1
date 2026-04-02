eb047b5544c8b4f7fb640254c13d2a702418abdc
nicolasmcmory <nicolasmcmory@gmail.com>
2026-04-02 14:01:40 -0400

    Update database settings to use PostgreSQL and modify requirements for Django version

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_test',
+        'USER': 'nico',
+        'PASSWORD': '1988',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
