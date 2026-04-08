e7759a9a05dafdeec017d321aa2190ff8ab16068
nicolasmcmory <nicolasmcmory@gmail.com>
2026-04-08 18:35:11 -0400

    Progress

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from portal.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    
+admin.site.register(User, UserAdmin)
